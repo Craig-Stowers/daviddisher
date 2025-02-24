7c58f9a23875c5927d30ca96248675cfa7886608
Craig Stowers <craig.stowers@outlook.com>
2025-02-24 17:06:02 +1100

    .

---
+++ src/app/(frontend)/bio/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default async function BioPage() {
+  return (
+    <div className="biopage">
+      <div className="content">
+        <h2>Here is the bio</h2>
+      </div>
+    </div>
+  )
+}
+
+export const revalidate = 120 // Cache the page for 60 seconds before refreshing
