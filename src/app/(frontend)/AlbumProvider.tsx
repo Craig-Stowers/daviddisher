acc81a3c04f7fa6903db18d4695414d0c2eca2f3
Craig Stowers <craig.stowers@outlook.com>
2025-02-27 02:17:42 +1100

    .

--- src/app/(frontend)/AlbumProvider.tsx
+++ src/app/(frontend)/AlbumProvider.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext } from 'react'
 
 // Define type for image items
 export interface ImageItem {
