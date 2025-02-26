c656d913656f5d9c241981de6dde97fa4fb72c27
Craig Stowers <craig.stowers@outlook.com>
2025-02-26 23:14:34 +1100

    .

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import './styles.css'
 
-import { AlbumProvider } from './AlbumProvider'
-
 export const metadata = {
   description: 'Art by David Disher',
   title: 'David Disher Art',
@@ -27,12 +25,10 @@ export default async function RootLayout(props: {
   return (
     <html>
       <body>
-        <AlbumProvider>
-          {props.children}
-          BELLOW CHILDREN
-          {props.modal}
-          <div id="modal-root" />
-        </AlbumProvider>
+        {props.children}
+        BELLOW CHILDREN
+        {props.modal}
+        <div id="modal-root" />
       </body>
     </html>
   )
