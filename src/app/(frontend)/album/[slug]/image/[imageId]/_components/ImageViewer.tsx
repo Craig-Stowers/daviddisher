665817820845971bdd7ab2a0fa34676162013f5e
Craig Stowers <craig.stowers@outlook.com>
2025-02-27 20:20:49 +1100

    .

--- src/app/(frontend)/album/[slug]/image/[imageId]/_components/ImageViewer.tsx
+++ src/app/(frontend)/album/[slug]/image/[imageId]/_components/ImageViewer.tsx
@@ -14,28 +14,59 @@ export default function ImageViewer({}) {
   // })
   // const [switchImage, setSwitchImage] = useState(false)
 
+  const [currentImageIndex, setCurrentImageIndex] = useState(null)
+  const [showImage, setShowImage] = useState(false)
+
   const { selectedIndex, setSelectedIndex, album } = useAlbum()
 
   useEffect(() => {
-    console.log('IMAGE VIEWER MOUNTED')
-    return () => {
-      console.log('IMAGE VIEWER UNMOUNTED')
+    let timer
+    if (currentImageIndex == null && selectedIndex != null) {
+      setCurrentImageIndex(selectedIndex)
+      timer = setTimeout(() => {
+        console.log('SHOW IT!')
+        setShowImage(true)
+      }, 260)
+
+      return () => {
+        clearTimeout(timer)
+      }
     }
-  }, [])
+    if (selectedIndex !== currentImageIndex) {
+      setShowImage(false)
+      timer = setTimeout(() => {
+        setCurrentImageIndex(selectedIndex)
+        setShowImage(true)
+      }, 260)
+
+      return () => {
+        clearTimeout(timer)
+      }
+      // setCurrentImageIndex(selectedIndex)
+    }
+
+    setShowImage(true)
+  }, [selectedIndex, album, currentImageIndex])
 
   useEffect(() => {
     console.log('IMAGE VIEWER album changed', album)
   }, [album])
 
-  if (selectedIndex == null) {
-    return null
-  }
-
-  const src = album.images[selectedIndex].url || '/fallback.jpg'
-  const alt = album.images[selectedIndex].alt || 'Default image'
+  const alt =
+    currentImageIndex !== null
+      ? album?.images[currentImageIndex].alt || 'Default image'
+      : 'Default image'
+  const src =
+    currentImageIndex !== null
+      ? album?.images[currentImageIndex].url || '/fallback.jpg'
+      : '/fallback.jpg'
 
   const nextIndex = selectedIndex < album.images.length - 1 ? selectedIndex + 1 : 0
   const prevIndex = selectedIndex > 0 ? selectedIndex - 1 : album.images.length - 1
+
+  const showViewer = selectedIndex != null
+
+  console.log('currentImageIndex', currentImageIndex, album, showImage)
 
   return (
     <div
@@ -46,17 +77,34 @@ export default function ImageViewer({}) {
         width: '100%',
         height: '100%',
         color: 'black',
-        transition: 'opacity 0.5s',
-        backgroundColor: 'rgba(255,255,255,0.8)',
-        pointerEvents: 'all',
+        transition: 'opacity 0.25s',
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        pointerEvents: showViewer ? 'all' : 'none',
+        opacity: showViewer ? 1 : 0,
       }}
     >
       IMAGE VIEWER HERE: {selectedIndex}
-      <Image src={src} layout="fill" objectFit="contain" alt={alt} />
-      <div style={{ position: 'absolute', bottom: 0, left: 0 }}>
+      <div
+        style={{
+          position: 'absolute',
+          width: '70%',
+          height: '70%',
+          opacity: showImage ? 1 : 0,
+          transition: 'opacity 0.25s',
+        }}
+      >
+        <Image src={src} layout="fill" objectFit="contain" alt={alt} />
+      </div>
+      <div style={{ position: 'absolute', bottom: 0, left: 0, color: 'white' }}>
         <Link href={`/album/${album.slug}/image/${prevIndex}`}>PREV IMAGE</Link>
         <Link href={`/album/${album.slug}/image/${nextIndex}`}>NEXT IMAGE</Link>
-        <Link href={`/album/${album.slug}`} onClick={() => setSelectedIndex(null)}>
+        <Link
+          href={`/album/${album.slug}`}
+          onClick={() => {
+            setShowImage(false)
+            setSelectedIndex(null)
+          }}
+        >
           CLOSE
         </Link>
       </div>
